9b1f7a7ae03d638ce8d9161b63bc1c9731ffd07d
Mario Vinti <mjvinti@gmail.com>
2023-05-30 15:30:31 -0400

    added app background

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 
@@ -15,7 +14,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>Coffee Connoisseur</h1>
       </main>
-      <footer className={styles.footer}></footer>
     </div>
   );
 }
