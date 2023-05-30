28786b04736dd2b4e71e50eecfe003e2fc42aed7
Mario Vinti <mjvinti@gmail.com>
2023-05-30 15:49:28 -0400

    added banner component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Banner from "@/components/Banner";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const onClick = () => console.log("hi banner button");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className={styles.title}>Coffee Connoisseur</h1>
+        <Banner buttonText="View stores nearby" handleOnClick={onClick} />
       </main>
     </div>
   );
