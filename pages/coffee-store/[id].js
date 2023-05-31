f0fadb0f17719c301697161ca3b87928243ec9be
Mario Vinti <mjvinti@gmail.com>
2023-05-31 17:22:06 -0400

    added route using link component

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,15 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log("router", router);
-  return <div>Coffee Store Pagr {router.query.id}</div>;
+  return (
+    <div>
+      Coffee Store Pagr {router.query.id}
+      <Link href="/">Back to home</Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
