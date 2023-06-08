2dc610c475083b39e0c731209325d23c1899a46f
Mario Vinti <mjvinti@gmail.com>
2023-06-08 13:15:08 -0400

    implemented get static props and get static paths

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,14 +1,34 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const CoffeeStore = () => {
+import coffeeStoresData from "@/data/coffee-stores.json";
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStore: coffeeStoresData.find(
+        (store) => store.id.toString() === params.id
+      ),
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: "0" } }, { params: { id: "1" } }],
+    fallback: false,
+  };
+}
+
+const CoffeeStore = ({ coffeeStore }) => {
   const router = useRouter();
-  console.log("router", router);
   return (
     <div>
       Coffee Store Page {router.query.id}
       <Link href="/">Back to home</Link>
       <Link href="/coffee-store/dynamic">Go to page dynamic</Link>
+      <p>{coffeeStore.address}</p>
+      <p>{coffeeStore.name}</p>
     </div>
   );
 };
